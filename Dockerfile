9f3d6772c6858e64b4f30462c01f13d3f5b45da0
hettling <hannes.hettling@gmail.com>
2019-03-19 14:53:39 +0100

    adding nba specimen fetch

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,2 @@
 FROM python:3-onbuild
 CMD python api.py
-
--- api.py
+++ api.py
@@ -17,12 +17,18 @@ except Exception as e:
     print('Defaulting to ' + ip + ':' + port)
     print(e)
     
-
+    
+user_agent = "topstukken-api/0.0.0"
 base_url = "https://topstukken.naturalis.nl"
+nba_url = "http://api.biodiversitydata.nl/v2"
 
 @app.route('/getObject/<name>')
 def get_object(name):
-    return jsonify(get_specimen(name))
+    enrich_nba = False
+    if (request.args.get('nba')):
+        if (request.args.get('nba').lower() == 'true'):
+            enrich_nba=True
+    return jsonify(get_specimen(name, enrich_nba=enrich_nba))
     
 @app.route('/names')
 def names():
@@ -32,28 +38,44 @@ def names():
 
 @app.route('/objects')
 def objects():
-    topstukken = get_topstukken()
+    enrich_nba = False
+    if (request.args.get('nba')):
+        if (request.args.get('nba').lower() == 'true'):
+            enrich_nba=True
+    topstukken = get_topstukken(enrich_nba=True)
     return jsonify(topstukken)
 
-def get_specimen(name):
+def get_specimen(name, enrich_nba=False):
     t_url = base_url + "/object/" + name
     t_json = parse_json_from_html(t_url)
     specimen = t_json['specimen']
     ## we don't want the related objects
     specimen.pop('related')
+    if (enrich_nba):
+        specimen['nba_specimen'] = get_nba_specimen(specimen['registrationNumber'])
+
     return specimen
     
-def get_topstukken(enrich=True):
+def get_topstukken(enrich=True, enrich_nba=False):
     json_data = parse_json_from_html(base_url)
     topstukken = json_data['grid']['items']
     if (enrich):
         names = list(t['slug'] for t in topstukken)
-        topstukken = list(map(lambda x: get_specimen(x), names))
-    return topstukken
+        topstukken = list(map(lambda x: get_specimen(x, enrich_nba=enrich_nba), names))
     
+    return topstukken
+
+def get_nba_specimen(unitID):
+    result = []
+    url = nba_url + "/specimen/findByUnitID/" + unitID
+    resp = requests.get(url, headers={'User-Agent': user_agent})
+    if (resp.status_code >199 and resp.status_code <= 299):        
+        result = json.loads(resp.content.decode())
+    return result
+
 def parse_json_from_html(url):
-    resp = requests.get(url,headers={'User-Agent':'Mozilla/5.0'})
-    script_text = re.findall('(?si)<script type="text/javascript">(.*?)</script>', resp.content.decode())    
+    resp = requests.get(url,headers={'User-Agent': user_agent})
+    script_text = re.findall('(?si)<script type="text/javascript">(.*?)</script>', resp.content.decode())
     ## have to parse everything from var INITIAL_DATA
     initial_data = re.findall('.*INITIAL_DATA = (.*);', script_text[0])[0]
     json_data = json.loads(initial_data)
@@ -61,7 +83,3 @@ def parse_json_from_html(url):
 
 if __name__ == "__main__":
     app.run(host=ip, port=port)
-
-
-
-
